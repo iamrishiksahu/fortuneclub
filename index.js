1cafc05bc9ff4c5a0ed322f6a9f794b0d6f04135
Rishik Sahu <rishikkr@hotmail.com>
2024-03-19 01:39:47 +0530

    backend

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 const PORT = process.env.PORT || 5000
+
+require('./routes/routes.js')(app);
 
 
 app.listen(PORT, () => {
